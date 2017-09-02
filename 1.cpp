d3b9289a5ae6c91e6bb67d16cbad09891173abdb
Ramon L. Collazo <rlcmartis@gmail.com>
2017-09-02 15:40:42 -0400

    Problem 1: Actually... we just want the result. No need to be fancy here ;)

--- 1.cpp
+++ 1.cpp
@@ -7,13 +7,12 @@ int main(int argc,char *argv[]){
   //Iterate from the first known number divisible by either 3 or 5, print and store 
   for(int i=3; i<1000; i++){
     if(i%3==0 || i%5==0){
-      cout << i << endl;
       total += i;
     }
   }
   
   //Finally print the result and exit
-  cout << "The total of the sum is " << total << ".\n";
+  cout << total << endl;
 
   return 0;
 }
